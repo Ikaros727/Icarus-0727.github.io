16732d0a4a71d5ea8e3956610b03101966cec499
chenyingpeng <ffchenyingpeng@chinatelecom.cn>
2022-09-22 17:15:00 +0800

    feature: 自定义照片

--- tools/izhicheng/js/index.js
+++ tools/izhicheng/js/index.js
@@ -1,67 +1,24 @@
-import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
+import { createApp, ref } from '/tools/izhicheng/js/vue.js'
 
 createApp({
     data() {
         return {
-            stuInfo: {
-                name: "刻晴",
-                sno: "212206301",
-                major: "计算机工程系",
-                class: "2022软件工程1",
-            },
-            currentTime: {
-                hour: 0,
-                minute: 0,
-                second: 0,
-                millsecond: 0,
-            },
-            currentDate: "",
+            avatar: ref(null),
+            imgUrl: ""
         }
     },
-    created() {
-        this.setCurrentDate()
-        this.setCurrentTime()
+    mounted() {
     },
     methods: {
-        setAvatar() {
-            
-        },
-        setStuInfo() {
-
-        },
-        setCurrentDate() {
-            var date = new Date()
-            var year = date.getFullYear()
-            var month = date.getMonth() + 1
-            if (month < 10) {
-                month = "0" + month
+        submitAvatar() {
+            let file = this.$refs.avatar.files[0]
+            let fileReader = new FileReader();
+            fileReader.readAsDataURL(file)
+            fileReader.onloadend = e => {
+                this.imgUrl = e.target.result
+                localStorage.setItem('avatar', `background-image: url('${e.target.result}');`)
+                location.replace("/tools/izhicheng/pass.html")
             }
-            var day = date.getDate()
-            this.currentDate = year + "年" + month + "月" + day + "日"
-        },
-        setCurrentTime() {
-            setInterval(() => {
-                var date = new Date()
-                this.currentTime.hour = date.getHours()
-                this.currentTime.minute = date.getMinutes()
-                this.currentTime.second = date.getSeconds()
-                this.currentTime.millsecond = date.getMilliseconds()
-                if (this.currentTime.hour < 10) {
-                    this.currentTime.hour = "0" + this.currentTime.hour
-                }
-                if (this.currentTime.minute < 10) {
-                    this.currentTime.minute = "0" + this.currentTime.minute
-                }
-                if (this.currentTime.second < 10) {
-                    this.currentTime.second = "0" + this.currentTime.second
-                }
-                if (this.currentTime.millsecond < 10) {
-                    this.currentTime.millsecond = "00" + this.currentTime.millsecond
-                }
-                if (this.currentTime.millsecond < 100) {
-                    this.currentTime.millsecond = "0" + this.currentTime.millsecond
-                }
-            }, 17);
         }
     },
 }).mount('#app')
---
+++ tools/izhicheng/js/pass.js
@@ -0,0 +1,69 @@
+import { createApp } from '/tools/izhicheng/js/vue.js'
+
+createApp({
+    data() {
+        return {
+            avatar: `background-image: url('/tools/izhicheng/imgs/avatar_default.png');`,
+            stuInfo: {
+                name: "刻晴",
+                sno: "212206301",
+                major: "计算机工程系",
+                class: "2022软件工程1",
+            },
+            currentTime: {
+                hour: 0,
+                minute: 0,
+                second: 0,
+                millsecond: 0,
+            },
+            currentDate: "",
+        }
+    },
+    created() {
+        this.setAvatar()
+        this.setCurrentDate()
+        this.setCurrentTime()
+    },
+    methods: {
+        setAvatar() {
+            this.avatar = localStorage.getItem("avatar") ?? this.avatar
+        },
+        setStuInfo() {
+
+        },
+        setCurrentDate() {
+            var date = new Date()
+            var year = date.getFullYear()
+            var month = date.getMonth() + 1
+            if (month < 10) {
+                month = "0" + month
+            }
+            var day = date.getDate()
+            this.currentDate = year + "年" + month + "月" + day + "日"
+        },
+        setCurrentTime() {
+            setInterval(() => {
+                var date = new Date()
+                this.currentTime.hour = date.getHours()
+                this.currentTime.minute = date.getMinutes()
+                this.currentTime.second = date.getSeconds()
+                this.currentTime.millsecond = date.getMilliseconds()
+                if (this.currentTime.hour < 10) {
+                    this.currentTime.hour = "0" + this.currentTime.hour
+                }
+                if (this.currentTime.minute < 10) {
+                    this.currentTime.minute = "0" + this.currentTime.minute
+                }
+                if (this.currentTime.second < 10) {
+                    this.currentTime.second = "0" + this.currentTime.second
+                }
+                if (this.currentTime.millsecond < 10) {
+                    this.currentTime.millsecond = "00" + this.currentTime.millsecond
+                }
+                if (this.currentTime.millsecond < 100) {
+                    this.currentTime.millsecond = "0" + this.currentTime.millsecond
+                }
+            }, 17);
+        }
+    },
+}).mount('#app')
